7c43260e96ccaec514cee04141b030c251689838
Emil Skogsund <emil@skogsund.se>
2025-04-30 05:18:35 +0200

    feat: add goose.sh script for running goose agent with predefined message

--- scripts/goose.sh
+++ scripts/goose.sh
@@ -4,4 +4,4 @@
 
 # Execute the agent command
 # $1 is the message parameter passed to the script
-/home/skogix/.skogai/agents/goose-src/target/release/goose run --text "$1"
+bash /home/skogix/.skogai/agents/goose-src/target/release/goose run --text 'testmessage'
