8ed999782ecc1b2e5f379bedf31e7919f6e7f9f3
Emil Skogsund <emil@skogsund.se>
2025-04-30 05:11:05 +0200

    chore: add goose.sh script with hardcoded test message

--- scripts/goose.sh
+++ scripts/goose.sh
@@ -3,5 +3,4 @@
 # This file is managed by skogcli - manual changes may be overwritten
 
 # Execute the agent command
-# Don't use bash to execute a binary file
-/home/skogix/.skogai/agents/goose-src/target/release/goose run --text '{message}'
+bash /home/skogix/.skogai/agents/goose-src/target/release/goose run --text 'testmessage'
