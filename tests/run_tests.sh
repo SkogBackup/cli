6b704f45d87028e90c3addb81417a79d5affd01b
Emil Skogsund <emil@skogsund.se>
2025-07-21 05:43:17 +0200

    tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# Run all tests
+# Run all tests with test environment variables
+
+# Export test environment variables that should override JSON config
+export SKOGAI_TEST_SCRIPT_USER_SCRIPTS_DIR="/tmp/skogcli_test_env_scripts"
+export SKOGAI_TEST_SCRIPT_METADATA_DIR="/tmp/skogcli_test_env_metadata"
+
+# Run tests
 uv run pytest tests/ "$@"
